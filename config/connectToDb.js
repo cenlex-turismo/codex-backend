4c75ebbc5258416b00b2970bd1ce4a712cf4c9d6
root <celex.turismo@gmail.com>
2025-05-28 18:32:01 +0000

    Small change

--- config/connectToDb.js
+++ config/connectToDb.js
@@ -1,12 +1,12 @@
-if (process.env.NODE_ENV != "production") {
-    require("dotenv").config()
-}
+require("dotenv").config()
 
 const mongoose = require("mongoose");
 
 async function connectToDb() {
+    const dbUrl = process.env.NODE_ENV === "production" ? process.env.DB_URL_P : process.env.DB_URL_T;
+
     try {
-        await mongoose.connect(process.env.DB_URL);
+        await mongoose.connect(dbUrl);
         console.log("Connected to DB");
     }
     catch (err) {
